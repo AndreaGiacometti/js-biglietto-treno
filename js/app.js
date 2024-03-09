ff5a4740922ccce89e51967bf035cdd84b4caeb1
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-03-09 16:49:33 +0100

    ADD create html and js file and linked  the files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Calcolare il prezzo del biglietto del treno:
+// - il prezzo varia in base ai km e costa 0,21 euro a km
+// - ai <18 anni viene applicato uno sconto del 20%, ai >65 anni uno sconto del 40%
+
+// Chiedere il numero dei km da percorrere
+
+//chiedere l'età
+
+// calcolare il prezzo al km
+
+//calcolare il prezzo del biglietto senza sconti
+
+//impostare le variabili di sconto
+
+//calcolare il prezzo del biglietto applicando gli sconti
+
+//stampare il prezzo finale riducendo il numero di cifre decimali a 2
+
+// Stampare il prezzo del biglietto sulla viewport tramite collegamento a html
+
+
+
+
+
